2b681c96f88e60d0c45a7da75be82d30a2235140
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-05-13 22:18:03 -0300

    getting the form values

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+
+  models: {
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: "Criação de website",
-          amount: 500,
-          type: "deposit",
-          category: "work",
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
